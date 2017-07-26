ac35067cd0a333f28adb28d5e230a92a0bfd7199
SeongHo Kim <shkim@enu-tech.co.kr>
2017-07-26 14:27:43 +0900

    enuSpace Interface update

--- enuSpaceTensorflow/GlobalHeader.h
+++ enuSpaceTensorflow/GlobalHeader.h
@@ -527,6 +527,7 @@ struct FetchInfo
 	std::vector<ObjectInfo*> fetch_object;			// ClientSession 객체의 fetch_output에 연결된 객체의 리스트
 	std::vector<tensorflow::Output> fetch_outputs;	// ClientSession 객체의 fetch_output에 연결된 객체의 output 리스트
 	std::vector<tensorflow::Tensor> outputs;		// 실행 결과를 담는 변수.
+	std::vector<std::string> pin_names;				// 입력핀의 이름
 
 	public:FetchInfo()
 	{
