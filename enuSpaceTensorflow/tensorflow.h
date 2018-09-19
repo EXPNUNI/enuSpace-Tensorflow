aa86234ccae7b10c89d5beae8c5187bdda5c1306
BrightJoy <jwlee@enu-tech.co.kr>
2018-09-19 21:13:46 +0900

    enuSpace 구조체 업데이트 따른 패치.

--- enuSpaceTensorflow/tensorflow.h
+++ enuSpaceTensorflow/tensorflow.h
@@ -62,11 +62,60 @@ public:
 #define DEF_MAXTEXT_LEN							2048
 #define DEF_LABELTEXT_LEN						1024
 
+#include <vector>
+
+struct VariableItem
+{
+	int iType;							// 변수의 타입 (int, float, bool, double, string, struct)
+	CString strType;					// 변수의 타입 문자열 이름
+	int iTotalSize;						// 변수의 사이즈(배열 포함)
+	int iUnitSize;						// 변수한개의 사이즈.
+	CString strName;					// 변수의 이름
+	CString strInitial;					// 변수의 초기값
+	void* pStructItem;					// 타입정보가 구조체인경우, 해당 구조체 정보를 가리키는 인자.
+	CString strDims;					// 문자열의 DIMENSION 정보.
+	std::vector<int> Dims;				// 배열의 DIMENSION 정보.
+	CString strDescription;				// (UI)변수의 설명
+	CString strUnit;					// (UI)변수의 단위
+	bool bReadOnly;						// (UI)읽기모드
+	bool bVisibility;					// (UI)디스플레이 모드
+	byte color[3];						// (UI)색상
+	CString min;						// (UI)최소값
+	CString max;						// (UI)최대값
+public:VariableItem()
+{
+	iType = DEF_UNKNOWN;
+	iTotalSize = 0;
+	iUnitSize = 0;
+	pStructItem = NULL;
+	bReadOnly = false;
+	bVisibility = true;
+	color[0] = color[1] = color[2] = 0;
+}
+};
+// 구조체의 정보를 담고 있는 구조체.
+struct StructItem
+{
+	CString strName;					// 구조체의 이름.
+	CString strFile;					// 선언된 파일이름.
+	CString strDescription;				// 구조체의 설명.
+	int iSize;							// 구조체의 사이즈.
+	std::vector<VariableItem> varList;	// 변수 아이템의 리스트.
+	bool bEnuObject;					// 본 구조체가 상위 부모 클래스가 EnuObject인지 확인		
+	bool bEnuLink;						// 본 구조체가 상뷔 부모 클래스가 EnuLink인지 확인.
+public:StructItem()
+{
+	iSize = 0;
+	bEnuObject = false;
+	bEnuLink = false;
+}
+};
+
 struct arrayInfo
 {
-	int size;
-	int position;								// enuSpace 버젼 (added v3.15)
-	wchar_t dimension[DEF_LABELTEXT_LEN];
+	int size;									// 변수 배열의 개수		ex) 100 
+	int position;								// 배열 변수의 위치 정보	ex) 10
+	wchar_t dimension[DEF_LABELTEXT_LEN];		// 문자열 배열 정보		ex) [10][10]
 
 public:arrayInfo()
 {
@@ -78,18 +127,23 @@ public:arrayInfo()
 
 struct VariableStruct
 {
-	wchar_t name[DEF_NAME_LEN];
-	int     type;
-	void*   pValue;
-	wchar_t strValue[DEF_MAXTEXT_LEN];
-	arrayInfo array;
-
+	wchar_t name[DEF_NAME_LEN];				// 변수 이름.
+	int     type;							// 변수의 타입 (단일변수 또는 구조체)
+	void*   pValue;							// 변수의 주소
+	wchar_t strValue[DEF_MAXTEXT_LEN];		// 단일 변수에 대한 문자열 값
+	arrayInfo array;						// 배열 정보
+	StructItem* pStructItem;				// 구조체 선언정보
+	VariableItem* pVariableItem;			// 변수 구조체 정보.
+	int iSize;								// 변수의 메모리 사이즈
 public:VariableStruct()
 {
-	wcscpy_s(name, L"");			// [ver.56] add : 초기화 추가.
+	wcscpy_s(name, L"");
 	type = DEF_UNKNOWN;
 	pValue = NULL;
 	wcscpy_s(strValue, L"N/A");
+	pStructItem = NULL;
+	pVariableItem = NULL;
+	iSize = 0;
 }
 };
 
